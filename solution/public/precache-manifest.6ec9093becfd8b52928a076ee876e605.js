64580d9e43e2ec075f053c43a630e6b9bd25087f
sherry2610 <shaharyarkhalid2610@gmail.com>
2020-05-13 22:58:36 +0500

    make build

---
+++ solution/public/precache-manifest.6ec9093becfd8b52928a076ee876e605.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c946ea2e9922d5b8a2da2d078f4ee2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60b3ee3aa9d2ad9a8080",
+    "url": "/static/js/2.e061ab3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e061ab3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46490883aec139df42bf",
+    "url": "/static/js/main.5be64f0d.chunk.js"
+  },
+  {
+    "revision": "103a46e15f3cf815f81c",
+    "url": "/static/js/runtime-main.b7d1e5ee.js"
+  }
+]);
